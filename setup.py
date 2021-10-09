3e831313e59b7a535bf798b3983ff1128df9eeb2
Victor Skvortsov <vds003@gmail.com>
2021-10-09 13:55:19 +0500

    Bump version 0.1.4 -> 0.1.5

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open(os.path.join(BASE_DIR, 'README.md'), 'r') as f:
 
 setup(
     name='afaligner',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/r4victor/afaligner',
     author='Victor Skvortsov',
     author_email='vds003@gmail.com',
